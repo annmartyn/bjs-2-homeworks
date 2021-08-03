2da1672b5b3bc36350757734c5c7c2c0734984f4
annmartyn <yutamar@yandex.ru>
2021-08-03 21:46:34 +0300

    first task

--- 1.base-concepts/task.js
+++ 1.base-concepts/task.js
@@ -1,11 +1,21 @@
 function solveEquation(a, b, c) {
-  let arr;
-  // код для задачи №1 писать здесь
-  return arr; // array
-}
+  "use strict";
+  let arr = [];
+  let d = b**2 - 4*a*c;
+  if (d < 0) {
+  	arr = [];
+  } else if (d===0) {
+    let x = -b/2*a;
+    arr.push(x);
+  } else {
+  	let x1 = (-b + Math.sqrt(d) )/(2*a);
+  	let x2 = (-b - Math.sqrt(d) )/(2*a);
+  	arr.push(x1, x2)
+  };
+  return arr; 
+};
 
 function calculateTotalMortgage(percent, contribution, amount, date) {
-  let totalAmount;
 
   // код для задачи №2 писать здесь
 
