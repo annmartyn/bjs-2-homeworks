55fae8e16949c68225db5092c87cf907077ecf48
annmartyn <yutamar@yandex.ru>
2021-08-06 20:58:12 +0300

    task 2

--- 3.arrays/task.js
+++ 3.arrays/task.js
@@ -1,15 +1,27 @@
+function isSame(number, index, arr2) {
+  console.log(arr2.indexOf(number))
+  return index = arr2.indexOf(number);
+}
+
 function compareArrays(arr1, arr2) {
   let result;
-
-  // Ваш код
-
-  return result; // boolean
+  result = arr1.every(isSame, arr2);
+  return result; 
 }
 
 function advancedFilter(arr) {
-  let resultArr;
-
-  // Ваш код
-
+  let resultArr = arr.filter(isThree).filter(isPositive).map(multiplyByTen);
   return resultArr; // array
+}
+
+function isThree(number) {
+  return number % 3 === 0;
+}
+
+function isPositive(number) {
+  return number > 0;
+}
+
+function multiplyByTen(number) {
+  return number * 10;
 }
